3b909fcb121012d4bef3031e108148df2909180d
Danil <59434801+ytowka@users.noreply.github.com>
2022-04-05 14:58:42 +0300

    inital commit

---
+++ main.py
@@ -0,0 +1,14 @@
+import numpy as np
+import os
+
+from tflite_model_maker.config import ExportFormat
+from tflite_model_maker import model_spec
+from tflite_model_maker import object_detector
+
+import tensorflow as tf
+assert tf.__version__.startswith('2')
+
+tf.get_logger().setLevel('ERROR')
+from absl import logging
+logging.set_verbosity(logging.ERROR)
+
